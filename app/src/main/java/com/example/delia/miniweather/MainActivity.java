701c8410d77b9a67cd5de447be683cc9566bcf6d
delia <594269260@qq.com>
2017-09-21 16:07:55 +0800

    first commit

---
+++ app/src/main/java/com/example/delia/miniweather/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.delia.miniweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by delia on 21/09/2017.
+ */
+
+public class MainActivity extends Activity
+{
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
