b990c91acc23a6ef8c56166e63e7f6c0599738b2
delia <594269260@qq.com>
2017-09-28 13:22:10 +0800

    third commit

--- app/src/main/java/com/example/delia/miniweather/MainActivity.java
+++ app/src/main/java/com/example/delia/miniweather/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.delia.miniweather;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.delia.util.NetUtil;
 
 /**
  * Created by delia on 21/09/2017.
@@ -13,5 +17,18 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather_info);
+
+
+        if(NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE)
+        {
+            Log.d("myWeather" , "网络OK");
+            Toast.makeText(MainActivity.this , "网络OK" , Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            Log.d("myWeather" , "网络挂了");
+            Toast.makeText(MainActivity.this , "网络挂了" , Toast.LENGTH_LONG).show();
+        }
+
     }
 }
